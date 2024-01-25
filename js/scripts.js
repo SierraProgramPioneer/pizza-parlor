40555c852185e7328aa9419dd82f98c6e3d15561
SierraProgramPioneer <melissaparker520@gmail.com>
2024-01-25 11:45:25 -0800

    Take Order Details and add to customer Order to add to current Orders

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,32 @@
 // Business Logic for Pizza Shop
 function CurrentOrders() {
     this.orders = {};
-    this.currentOrderId = 1;
+    this.currentOrderId = 0;
+}
+
+CurrentOrders.prototype.addOrder = function (order) {
+    order.orderId = this.assignOrderId();
+    this.orders[order.orderId] = order;
+}
+
+CurrentOrders.prototype.assignOrderId = function () {
+    this.currentOrderId = + 1;
+    return this.currentOrderId;
 }
 
 // Business Logic for Customer's Order
-function customerOrder(orderId, customerName, orderType) {
-    this.orderId = orderId;
+function CustomerOrder(customerName, orderType) {
     this.customerName = customerName;
     this.pizzas = {};
     this.orderType = orderType;
 }
 
 // Business Logic for Pizza
-
+function Pizza() {
+    this.toppings = {};
+    this.size = size;
+    this.price = price;
+}
 
 // User Interface Logic
 let currentOrders = new CurrentOrders();
@@ -21,7 +34,10 @@ let currentOrders = new CurrentOrders();
 
 function handleOrder(event) {
     event.preventDefault();
-    console.log("Order Placed");
+    customerName = document.getElementById("customerName").value;
+    orderType = document.getElementById("orderType").value;
+    let customerOrder = new CustomerOrder(customerName, orderType);
+    currentOrders.addOrder(customerOrder);
 }
 
 window.addEventListener("load", function () {
