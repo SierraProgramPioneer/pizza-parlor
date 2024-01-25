1e49b60c9246246296b06cf2857e31dad3b9c9c1
SierraProgramPioneer <melissaparker520@gmail.com>
2024-01-25 11:16:08 -0800

    Add constructors for CurrentOrders & CustomerOrder

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
+// Business Logic for Pizza Shop
+function CurrentOrders() {
+    this.orders = {};
+    this.currentOrderId = 1;
+}
+
+// Business Logic for Customer's Order
+function customerOrder(orderId, customerName, orderType) {
+    this.orderId = orderId;
+    this.customerName = customerName;
+    this.pizzas = {};
+    this.orderType = orderType;
+}
+
+// Business Logic for Pizza
+
+
+// User Interface Logic
+let currentOrders = new CurrentOrders();
+
+
 function handleOrder(event) {
     event.preventDefault();
     console.log("Order Placed");
